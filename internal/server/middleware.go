f99add1cffd77542d3e8a470751713dac71c09cd
Sasa Milasinovic <sasa.milasinovic@gmail.com>
2022-04-15 10:13:19 +0300

    add json middleware

---
+++ internal/server/middleware.go
@@ -0,0 +1,13 @@
+package server
+
+import (
+	"net/http"
+)
+
+// JSONMiddleware - sets content type to application json
+func JSONMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		next.ServeHTTP(w, r)
+	})
+}
